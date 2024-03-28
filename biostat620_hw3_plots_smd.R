d97940509e8db1c4ed9d71916da3a0082987fcf3
Vikram Bala <145570139+lellerz@users.noreply.github.com>
2024-03-28 14:41:55 -0400

    update unlist() in hw3 pt3 function

--- biostat620_hw3_plots_smd.R
+++ biostat620_hw3_plots_smd.R
@@ -8,22 +8,22 @@ quality_plots_smd <- function(data, match_pairs, prop_scores, var_col)
   control_row <- sapply(match_pairs, function(x) x[2])
 
   # Find propensity scores for different groups
-  prop_score_control <- prop_scores[control_row]
-  prop_score_treat <- prop_scores[treatment_row]
+  prop_score_control <- prop_scores[unlist(control_row)]
+  prop_score_treat <- prop_scores[unlist(treatment_row)]
 
   # Make paired histogram for the two groups
   hist_prop_score_control <- hist(prop_score_control)
   hist_prop_score_treat <- hist(prop_score_treat)
   hist_prop_score_control$counts <- -hist_prop_score_control$counts
-  plot(hist_prop_score_treat, xlim=c(0,1), ylim=c(-5,5),
+  plot(hist_prop_score_treat, xlim=c(0,1), ylim=c(-100,100),
        main="Histograms of Propensity Scores by Treatment",
        xlab="Propensity Scores", col="blue")
   lines(hist_prop_score_control, col="red")
   legend("topright", c("Treatment", "Control"), fill=c("blue", "red"), cex=0.5)
 
   # Split variable of interest based on groups
-  var_control <- data[, var_col][control_row]
-  var_treat <- data[, var_col][treatment_row]
+  var_control <- data[, var_col][unlist(control_row)]
+  var_treat <- data[, var_col][unlist(treatment_row)]
 
   # Calculate and return SMD based on variable values
   numer <- abs(mean(var_treat) - mean(var_control))
